41d1766561e744cce2fbc5e8267d23690d5ef4be
Blessing Hirwa <hirwablessing7@gmail.com>
2022-02-22 07:28:42 +0200

    chore: project setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
